f966d6ecc9a3d5b383d7c29b6267ce8ec6e52c90
Bryan K <78167383+BryanProg@users.noreply.github.com>
2021-04-22 16:37:10 -0300

    Error code
    File for Error Code Settings

---
+++ Data structure/Singly Linked List/header/Error_code.h
@@ -0,0 +1,12 @@
+#ifndef ERROR_CODE_H
+    #define ERROR_CODE_H
+
+    typedef enum ERRC_CODE
+    {
+        LL_EC_SUCESS,
+        LL_EC_INSERTION
+    } Errc_code_t;
+    
+    //To implement functions
+
+#endif
